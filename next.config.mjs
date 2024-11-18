2c473f33285ed9dd43771410a241fbe59db1096a
Kiara Monahan <142260356+kiaramonahan@users.noreply.github.com>
2024-11-17 17:15:45 -0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export", // Enables static export for GitHub Pages
+  images: {
+    unoptimized: true, // Disables Next.js image optimization for static hosting
+  },
+};
 
 export default nextConfig;
+

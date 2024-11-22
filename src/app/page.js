47ddfe466a488a1b6e2f4c177dd46534e9f5cd13
KIARA MONAHAN <kmonahan@berkeley.edu>
2024-11-21 17:51:56 -0800

    updates

--- src/app/page.js
+++ src/app/page.js
@@ -6,16 +6,8 @@ export default function Home() {
       <div className="header-container">
         <Header />
       </div>
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "space-between",
-          padding: "20px",
-        }}
-      >
-        {/* Text Section */}
-        <div style={{ maxWidth: "50%", color: "white", padding: "20px" }}>
+      {/* Text Section */}
+      <div style={{ maxWidth: "50%", color: "white", padding: "20px" }}>
           <h1 style={{ fontSize: "3rem", fontWeight: "bold", whiteSpace: "nowrap", marginBottom: "20px" }}>
             Welcome to CoralSense
           </h1>
@@ -23,15 +15,8 @@ export default function Home() {
           "Coral reefs are a jeweled belt around the middle of the planet." - Dr. Sylvia Earle, Oceanographer
           </p>
         </div>
-
-        {/* Image Section */}
-        <div>
-          <img
-            src="home_page_image.png"
-            alt="Image of coral reef"
-            style={{ maxWidth: "100%", height: "auto", borderRadius: "10px" }}
-          />
-        </div>
+      <div className="background-image">
+        <img src="home_page_image.png" alt="Image of coral reef" />
       </div>
     </div>
   );
